d4c0882810f2659d491f91093bc300ce96fc5d69
jcha0706 <105154862+jcha0706@users.noreply.github.com>
2022-05-11 15:08:02 +0900

    Add files via upload
    로그인, 회원가입 업데이트

--- app.py
+++ app.py
@@ -1,37 +1,101 @@
-from flask import Flask, render_template, request, jsonify
-
-
+from flask import Flask, render_template, request, jsonify, url_for, redirect
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-
+import requests
+from bs4 import BeautifulSoup
 
 from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+from datetime import datetime, timedelta
 import certifi
 
 ca = certifi.where()
+
+SECRET_KEY = 'SPARTA'
 client = MongoClient('mongodb+srv://wea9677:tmxkdlfl@cluster0.xmzro.mongodb.net/Cluster0?retryWrites=true&w=majority',  tlsCAFile=ca)
 
 db = client.dbsparta
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-
+#-----login, register-----
 
 @app.route('/login')
-def detail():
-    return render_template("login.html")
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
 
 
 
 
 
+#-------main-------
 
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
 
-
+        return render_template('index.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    return render_template('index.html')
 
 @app.route('/culture/update')
 def update():
@@ -65,7 +129,6 @@ def web_culture_post():
         count = len(culture_list) + 1
     else:
         count = culture_list[len(culture_list) - 1]['num'] + 1
-
     doc = {
         'num': count,
         'title': title_recevie,
@@ -85,12 +148,11 @@ def web_culture_get():
 
     return jsonify({'posting': post_list})
 
-
 @app.route('/culture/ctype', methods=['GET'])
 def web_culture_gettype():
     ctype = request.args.get('str')
     post_list = list(db.culture.find({'ctype': ctype}, {'_id': False}))
-    print(post_list)
+
     return jsonify({'posting': post_list})
 
 @app.route("/culture", methods=["PUT"])
